c9e9dd1740865a7de11ae927b5ee6af0c450ea00
Fouad Saoudi <fsaoudi@nuundigital.com>
2025-04-29 21:51:55 +0300

    Add variables to .env and fixing database connection

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,10 +1,10 @@
 module.exports = {
     client: 'mysql2',
     connection: {
-        host: 'localhost',
-        user: 'root',
-        password: '@l8i1tUre',
-        database: 'dubizzle_db',  // Default database that will be used in migrations
+        host: process.env.DB_HOST,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,  // Default database that will be used in migrations
     },
     migrations: {
         directory: './migrations',  // Directory where migration files are stored
