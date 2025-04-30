6112038b635b768edaffc786398bc3bbd170271e
Fouad Saoudi <fsaoudi@nuundigital.com>
2025-05-01 01:59:42 +0300

    add a server route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = 8080;
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const router = express.Router();
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,7 +19,10 @@ app.use('/api', adRoutes);
 app.use('/api', authRoutes);
 app.use('/api', adminAdRoutes);
 app.use('/api', subCategoriesRoute);
-
+router.get('/', (req, res) => {
+    res.json({ message: 'hello' });
+});
+app.use('/api', router);
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
